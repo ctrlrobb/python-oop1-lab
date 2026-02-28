7e6d7899d568eb4291319923b388595db0e69e34
ctrlrobb <robyampoy@gmail.com>
2026-02-28 17:16:37 +0300

    Add files via upload
    Done

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self._size = size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        allowed_sizes = ["Small", "Medium", "Large"]
+        if value not in allowed_sizes:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1.0
